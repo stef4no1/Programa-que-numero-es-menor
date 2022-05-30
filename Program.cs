60e59f800b0271f79c9fc88236615b8862dfeae6
Stefano Straga <stefanostraga2k18@gmail.com>
2022-05-30 17:18:14 -0300

    subiendo el programa finalizado

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, menor;
+
+            Console.WriteLine("Ingrese un numero");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero");
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el ultimo numero");
+            d = int.Parse(Console.ReadLine());
+
+            if ( a < b ) {
+                menor = a;
+            } else {
+                menor = b;
+            }
+            if ( c < menor ) {
+                menor = c;
+            } 
+            if ( d < menor) {
+                menor = d;
+            }
+            Console.WriteLine("el valor menor es : " + menor);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Programa-que-numero-es-menor
